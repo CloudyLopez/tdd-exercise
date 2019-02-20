1e1173054dd46c6c8273c4fc303a27982d337476
Cloudy Lopez <cloudylopez@gmail.com>
2019-02-20 14:06:40 -0800

    try 4

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,25 @@
 # blackjack_score.rb
 
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
+FACECARDS = ["King", "Queen", "Jack"]
 
 def blackjack_score(hand)
+    score = 0
+    aces = 0
 
-end
+    hand.each do |card|
+        unless VALID_CARDS.include? card
+            raise ArgumentError, "#{card} is an invaliud card"
+        end
+    
+        if FACECARDS.include?(card)
+            score += 10
+        elsif card == 1
+            score += 1
+            aces += 1
+        else score += card
+        end
+    end
+
+        if score <= 11 &&
+#rtrrrrry 4
